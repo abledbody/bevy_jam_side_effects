45e4579027370ebc235e3fa98b5a9ffcad9e3371
Ben Frankel <ben.frankel7@gmail.com>
2023-04-01 14:37:21 -0700

    Set up some bevy basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use bevy::prelude::*;
+
+const TITLE: &'static str = "My Title";
+const CLEAR_COLOR: Color = Color::AQUAMARINE;
+
 fn main() {
-    println!("Hello, world!");
+    // Hot reload assets
+    #[cfg(feature = "debug_mode")]
+    let watch_for_changes = true;
+    #[cfg(not(feature = "debug_mode"))]
+    let watch_for_changes = false;
+
+    let mut app = App::new();
+
+    // Resources
+    app.insert_resource(ClearColor(CLEAR_COLOR));
+
+    // Plugins
+    app.add_plugins(
+        DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: TITLE.to_string(),
+                    ..default()
+                }),
+                ..default()
+            })
+            .set(AssetPlugin {
+                watch_for_changes,
+                ..default()
+            })
+            .set(ImagePlugin::default_nearest()),
+    );
+
+    // Startup systems
+    app.add_startup_system(spawn_camera);
+
+    // UI systems
+    app.add_system(bevy::window::close_on_esc);
+
+    app.run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
