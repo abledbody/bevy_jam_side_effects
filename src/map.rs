8d05b78ca3c7cfc6399715d0709cd8ac0debdcca
Alex Coats <alexandcoats@gmail.com>
2023-04-03 17:52:26 -0400

    Use new tileset and build colliders from tile enum

--- src/map.rs
+++ src/map.rs
@@ -10,42 +10,34 @@ impl Plugin for MapPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(LdtkPlugin)
             .insert_resource(LevelSelection::Index(0))
-            .register_ldtk_int_cell::<WallBundle>(2);
+            .add_system(spawn_walls);
     }
 }
 
 #[derive(Component, Reflect, Copy, Clone, Eq, PartialEq, Debug, Default)]
 pub struct Wall;
 
-#[derive(Bundle, Clone, Debug)]
-pub struct WallCollider {
+#[derive(Clone, Default, Debug, Bundle)]
+pub struct WallBundle {
+    wall: Wall,
     collider: Collider,
     collision_groups: CollisionGroups,
     rigid_body: RigidBody,
     friction: Friction,
 }
 
-#[derive(Clone, Debug, Bundle, LdtkIntCell)]
-pub struct WallBundle {
-    wall: Wall,
-    #[bundle]
-    #[from_int_grid_cell]
-    collider: WallCollider,
-}
-
-impl From<IntGridCell> for WallCollider {
-    fn from(_value: IntGridCell) -> Self {
-        Self {
-            collider: Collider::cuboid(8.0, 8.0),
-            collision_groups: CollisionGroups {
-                memberships: COLLISION_GROUP,
-                filters: COLLISION_GROUP,
-            },
-            rigid_body: RigidBody::Fixed,
-            friction: Friction {
-                coefficient: 1.0,
+fn spawn_walls(mut commands: Commands, tags: Query<(Entity, &TileEnumTags), Added<TileEnumTags>>) {
+    for (entity, tag) in &tags {
+        if tag.tags.iter().find(|s| s.as_str() == "Full").is_some() {
+            commands.entity(entity).insert(WallBundle {
+                collider: Collider::cuboid(8.0, 8.0),
+                collision_groups: CollisionGroups {
+                    memberships: COLLISION_GROUP,
+                    filters: COLLISION_GROUP,
+                },
+                rigid_body: RigidBody::Fixed,
                 ..default()
-            },
+            });
         }
     }
 }
