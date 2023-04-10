01767b0e57c46dac9bec1f59919732f9474aaf82
Ben Frankel <ben.frankel7@gmail.com>
2023-04-09 20:51:25 -0700

    Add wasm-opt step to build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ rm -rf wasm wasm.zip
 wasm-bindgen --no-typescript --out-name bevy_game --out-dir wasm --target web target/wasm32-unknown-unknown/release/bevy_jam_side_effects.wasm
 cp -r assets web/* wasm
 rm wasm/**/*.aseprite
-zip --recurse-paths wasm.zip wasm
+wasm-opt -O -ol 100 -s 100 -o wasm/bevy_game_bg.wasm wasm/bevy_game_bg.wasm
+zip -r wasm.zip wasm
 
 exit 0
